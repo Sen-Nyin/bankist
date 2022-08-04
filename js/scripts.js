84a535702c48eb57c6306c654fda208739b4af42
Leon Lonsdale <leonlonsdale@icloud.com>
2022-08-04 19:59:54 +0100

    starter data added

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const currencies = new Map([
+  ["USD", "United States Dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound Stirling"],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
