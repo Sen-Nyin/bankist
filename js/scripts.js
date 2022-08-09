48beedffb291ad1674ea3f078068b6b7f73427c8
Leon Lonsdale <leonlonsdale@icloud.com>
2022-08-09 18:46:23 +0100

    all figures rounded

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,22 @@
 // Data
 const account1 = {
   owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-07-26T17:01:17.194Z",
+    "2020-07-28T23:36:17.929Z",
+    "2020-08-01T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -17,6 +30,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account3 = {
@@ -73,7 +99,7 @@ const displayMovements = function (movements, sort = false) {
           <div class="movements__type movements__type--${type}">${
       index + 1
     } ${type}</div>
-          <div class="movements__value">€${Math.abs(mov)}</div>
+          <div class="movements__value">€${mov.toFixed(2)}</div>
         </div>`;
 
     containerMovements.insertAdjacentHTML("afterbegin", html);
@@ -101,7 +127,7 @@ createUsername(accounts);
 
 const calcDisplayBalance = (acc) => {
   acc.balance = acc.movements.reduce((acc, trans) => acc + trans, 0);
-  labelBalance.textContent = `€${acc.balance}`;
+  labelBalance.textContent = `€${acc.balance.toFixed(2)}`;
 };
 
 // get total of deposits
@@ -110,21 +136,23 @@ const calcDisplaySummary = (account) => {
   // Deposits
   labelSumIn.textContent = `€${account.movements
     .filter((movement) => movement > 0)
-    .reduce((acc, curr) => (acc = curr))}`;
+    .reduce((acc, curr) => (acc = curr))
+    .toFixed(2)}`;
 
   // Credits
   labelSumOut.textContent = `€${Math.abs(
     account.movements
       .filter((movement) => movement < 0)
       .reduce((acc, curr) => acc + curr)
-  )}`;
+  ).toFixed(2)}`;
 
   // Interest - paid on deposit, if interest is at least 1
   labelSumInterest.textContent = `€${account.movements
     .filter((movement) => movement > 0)
     .map((deposit) => deposit * (account.interestRate / 100))
     .filter((interest) => interest >= 1)
-    .reduce((acc, interest) => acc + interest)}`;
+    .reduce((acc, interest) => acc + interest)
+    .toFixed(2)}`;
 };
 
 const updateUI = (currentAccount) => {
@@ -186,7 +214,7 @@ btnTransfer.addEventListener("click", (e) => {
 
 btnLoan.addEventListener("click", (e) => {
   e.preventDefault();
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
   if (
     amount > 0 &&
     currentAccount.movements.some((movement) => movement >= amount * 0.1)
